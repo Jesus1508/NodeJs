7c6c8d000757498aeb3d4a74d828e8fe01bc2a8b
Carmen de Jesus Solis Martinez <iscjesus1508@gmail.com>
2022-07-05 23:50:41 -0500

    Repaso JavaScript 2

--- 02-fundamentos/async-await.js
+++ 02-fundamentos/async-await.js
@@ -47,34 +47,31 @@ const getSalario = (id, callback) => {
     return promesa;
 }
 
-const id = 3
+const getInfoUsuario = async(id) => {
+    
+    try{
+        const empleado= await getEmpleado(id);
 
-// getEmpleado(id)
-//     .then( empleado => console.log(empleado))
-//     .catch( err => console.log(err));
+        const salario= await getSalario(id);
 
-// getSalario(id)
-//     .then( salario => console.log(salario))
-//     .catch( err => console.log(err));
+        return `El salario del empleado: ${empleado} es de ${salario}`;
+    }catch(error){
+        throw error;
+    }
+    
+}
 
-// getEmpleado(id)
-//     .then ( empleado => {
-//         getSalario(id)
-//             .then(salario => {
-//                 console.log('Él empleado: ', empleado, ' tiene un salario de:', salario)
-//             })
-//             .catch( err => console.log(err))
-//     })
-//     .catch( err => console.log(err))
+const id = 10;
 
-//Promesas en cadena
-
-let nombre;
-
-getEmpleado(id)
-    .then(empleado => {
-        nombre = empleado;
-        return getSalario(id)
+getInfoUsuario(id)
+    .then(msg => {
+        console.log("TODO BIEN!!!");
+        console.log(msg)
     })
-    .then( salario => console.log('El empleado: ', nombre, ' tiene un salario de: ', salario))
-    .catch( err => console.log(err)) 
+    .catch(err => {
+        console.log("ERROR!!!")
+        console.log(err)
+    });
+
+
+
